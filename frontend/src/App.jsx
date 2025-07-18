df64fdb2e2601ecca5d920b8350aa93f26842a69
Tisha <127007536+Tisha7353@users.noreply.github.com>
2025-07-18 12:58:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import Login from './pages/auth/login';
-import Signup from './pages/auth/signup';
+import Signup from './pages/auth/Signup';
 import LandingPage from './pages/landingPage';
 import Dashboard from './pages/home/Dashboard';
 import InterviewPrep from './pages/interviewPrep/interviewPrep';
