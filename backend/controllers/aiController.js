bc5460a15f793b4d4faca76e2aa4c9a2602e762c
Tisha <127007536+Tisha7353@users.noreply.github.com>
2025-11-19 19:28:00 +0530

    Update AI model to 'gemini-1.5-flash-latest'

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -6,7 +6,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+
 
 export const generateInterviewQuestions = async (req, res) => {
   let text="";
